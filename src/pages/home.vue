0bff76accd54102376770928dd4c1a16db67f70b
lafitewu <863512936@qq.com>
2019-08-16 09:21:25 +0800

    vue Pug模板提交

---
+++ src/pages/home.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+    //- 测试
+    .home {{title}}
+        h1 555
+        van-button(type="warning" class="btn") 危险按钮
+        .icon
+          van-cell(title="单元格" value="内容" icon="location-o")
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style lang="less">
+@testcolor: blue;
+    .home {
+        color: @testcolor;
+    };
+    .btn {
+        color: @testcolor;
+    }
+    .icon .van-cell{
+      background: pink;
+    }
+      .van-cell__title {
+        text-align: left;
+      }
+</style>
